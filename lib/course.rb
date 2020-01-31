4c39a21bc552552d421e6f327a71eca6f6ab15c3
Austin Johnson <technicallyaustin@gmail.com>
2020-01-31 05:48:16 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,15 +1,13 @@
 class Course
   attr_accessor :title, :schedule, :description 
   @@all = []
-  def initialize(title)
-    @title = title 
-    @schedule = schedule 
-    @description = descritption 
+  def initialize
+    @@all << self 
   end
   
   
   def self.reset_all 
-    @@all = []
+    @@all.clear
   end
   
   def self.all
