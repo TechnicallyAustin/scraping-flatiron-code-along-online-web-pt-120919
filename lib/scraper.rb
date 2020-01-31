080f1d789aa2ce197cfed3b7bf7aa8fdd918204c
Austin Johnson <technicallyaustin@gmail.com>
2020-01-31 06:18:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,7 @@ class Scraper
   end
   
   def make_courses
-    self.css(".post").each do |post|
+    self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2")
       course.schedule = post.css(".date")
